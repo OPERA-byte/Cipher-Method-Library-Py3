2fa91c1fa4a6191363f8d7967909e6649a851aa7
Pyramids <135397750+OPERA-byte@users.noreply.github.com>
2024-05-26 22:38:34 -0400

    Update Vigenere.py
    Comments

--- Vigenere.py
+++ Vigenere.py
@@ -1,14 +1,41 @@
 
 
 class Vigenere:
+    """
+       Vigenere cipher implementation.
+
+       Attributes:
+           dictionary_key (str): The key used to prepend to the base alphabet.
+           base_alphabet (list): The base alphabet modified with dictionary key characters prepended.
+           ciphered_dictionary (list): The Vigenere table containing the ciphered alphabet.
+           key_stream (str): The formatted key stream matching the length of the plaintext.
+
+    """
     dictionary_key = ''
     base_alphabet = []
     ciphered_dictionary = []
     key_stream = ''
 
     def __init__(self, dictionary_key='SATOR'):
+        """
+                Initialize the Vigenere cipher object with a dictionary key.
+
+                Args:
+                    dictionary_key (str, optional): The key to prepend to the base alphabet.
+                        Defaults to 'SATOR'.
+
+        """
         self.dictionary_key = dictionary_key
     def preapend_dictionary_key_to_base_alphabet(self, dictionary_key):
+        """
+            Prepend the characters of the dictionary key to the base alphabet.
+
+            Args:
+                dictionary_key (str): The key to prepend to the base alphabet.
+
+            Returns:
+                list: The modified base alphabet with dictionary key characters prepended.
+        """
         alphabet = (["A", "B", "C", "D", "E", "F",
                      "G", "H", "I", "J", "K", "L", "M",
                      "N", "O", "P", "Q", "R", "S", "T",
@@ -21,10 +48,22 @@ class Vigenere:
         return alphabet
 
     def print_vigenere_table(self):
+        """
+            Print the Vigenere table.
+
+            Prints each row of the Vigenere table contained in self.ciphered_dictionary.
+
+        """
         for i in range(len(self.ciphered_dictionary)):
             print("     " + self.ciphered_dictionary[i])
 
     def construct_vigenere_table(self, base_alphabet):
+        """
+            Construct the Vigenere table using the provided base alphabet.
+
+            Args:
+                base_alphabet (list): The base alphabet used to construct the Vigenere table.
+        """
         self.ciphered_dictionary.clear()
         temp_character = ''
         self.ciphered_dictionary.append(''.join(base_alphabet))
@@ -38,11 +77,30 @@ class Vigenere:
         self.print_vigenere_table()
 
     def new_vigenere_table(self, dictionary_key='GLYPH'):
+        """
+            Create a new Vigenere table using the specified dictionary key.
+
+            Args:
+                dictionary_key (str, optional): The key to prepend to the base alphabet.
+                    Defaults to 'GLYPH'.
+
+        """
         self.dictionary_key = dictionary_key
         self.base_alphabet = self.preapend_dictionary_key_to_base_alphabet(self.dictionary_key)
         self.construct_vigenere_table(self.base_alphabet)
 
     def format_key_stream(self, key_stream, plain_text):
+        """
+            Format the key stream to match the length of the plaintext.
+
+            Args:
+                key_stream (str): The key stream.
+                plain_text (str): The plaintext.
+
+            Returns:
+                str: The formatted key stream.
+
+        """
         plain_text_length = len(plain_text)
         key_stream_length = len(key_stream)
         temp_character = ''
@@ -50,35 +108,6 @@ class Vigenere:
             temp_character = key_stream[i]
             key_stream += temp_character
         return key_stream
-
-    # def encrypt(self, temp_key_stream, plain_text):
-    #     self.key_stream = self.format_key_stream(temp_key_stream, plain_text)
-    #     temp_plain_text_char = ''
-    #     plain_text_int = -1
-    #     key_stream_int = -1
-    #     temp_cipher_text = ''
-    #     cipher_text_arr = []
-    #     for i in range(len(plain_text)):
-    #         if not plain_text[i].isalpha():
-    #             cipher_text_arr.append(plain_text[i])
-    #         else:
-    #             plain_text_int = self.ciphered_dictionary[0].index(plain_text[i])
-    #             key_stream_int = self.ciphered_dictionary[0].index(self.key_stream[i])
-    #             temp_cipher_text = self.ciphered_dictionary[plain_text_int][key_stream_int]
-    #             cipher_text_arr.append(temp_cipher_text)
-    #     return ''.join(cipher_text_arr)
-
-
-
-
-
-
-# program = Vigenere()
-# # base_alphabet = program.construct_vigenere_table(alphabet)
-# # program.new_vigenere_table()
-# word = program.encrypt("KEYWORD", "SECRET MESSAGE")
-# print(word)
-
 
 
 
